dd3f1c214428732132c9b2b60a12d8226554d3ea
jamil <jamil.saleh@student.moringaschool.com>
2023-02-10 23:25:37 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet (name)
+    puts "Hello, #{name}!"
+end 
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(numA, numB) 
+    return numA + numB
+end
+
+def halve(numX)
+    if numX.class != Integer
+        return nil
+end
+    return numX / 2
+end
